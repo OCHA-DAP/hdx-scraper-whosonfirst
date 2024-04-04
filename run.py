1a127470fbc0ad4d99bc75bec5e20b8cfae01aca
Briar <briar.j.mills@gmail.com>
2024-04-04 14:55:04 -0400

    Initial commit of pipeline

---
+++ run.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+"""
+Top level script. Calls other functions that generate datasets that this script then creates in HDX.
+
+"""
+import logging
+from copy import deepcopy
+from os.path import expanduser, join
+
+from hdx.api.configuration import Configuration
+from hdx.data.hdxobject import HDXError
+from hdx.facades.infer_arguments import facade
+from hdx.utilities.downloader import Download
+from hdx.utilities.errors_onexit import ErrorsOnExit
+from hdx.utilities.path import progress_storing_folder, wheretostart_tempdir_batch
+from hdx.utilities.retriever import Retrieve
+from hdx.utilities.state import State
+
+from whosonfirst import WhosOnFirst
+
+logger = logging.getLogger(__name__)
+
+lookup = "hdx-scraper-whosonfirst"
+updated_by_script = "HDX Scraper: Who's On First"
+
+
+def main(save: bool = False, use_saved: bool = False) -> None:
+    """Generate datasets and create them in HDX"""
+    with ErrorsOnExit() as errors:
+        with State(
+                "dataset_dates.txt",
+                State.dates_str_to_country_date_dict,
+                State.country_date_dict_to_dates_str,
+        ) as state:
+            state_dict = deepcopy(state.get())
+            with wheretostart_tempdir_batch(lookup) as info:
+                folder = info["folder"]
+                with Download() as downloader:
+                    retriever = Retrieve(
+                        downloader, folder, "saved_data", folder, save, use_saved
+                    )
+                    folder = info["folder"]
+                    batch = info["batch"]
+                    configuration = Configuration.read()
+                    whosonfirst = WhosOnFirst(configuration, retriever, folder, errors)
+                    dataset_names, state_dict = whosonfirst.get_data(state_dict)
+                    logger.info(f"Number of datasets to upload: {len(dataset_names)}")
+
+                    for _, nextdict in progress_storing_folder(info, dataset_names, "name"):
+                        dataset_name = nextdict["name"]
+                        dataset = whosonfirst.generate_dataset(dataset_name)
+                        if dataset:
+                            dataset.update_from_yaml()
+                            dataset["notes"] = dataset["notes"].replace(
+                                "\n", "  \n"
+                            )  # ensure markdown has line breaks
+                            try:
+                                dataset.create_in_hdx(
+                                    remove_additional_resources=True,
+                                    hxl_update=False,
+                                    updated_by_script=updated_by_script,
+                                    batch=batch,
+                                    ignore_fields=["resource:description", "extras"],
+                                )
+                            except HDXError:
+                                errors.add(f"Could not upload {dataset_name}")
+                                continue
+            state.set(state_dict)
+
+
+if __name__ == "__main__":
+    facade(
+        main,
+        user_agent_config_yaml=join(expanduser("~"), ".useragents.yaml"),
+        user_agent_lookup=lookup,
+        project_config_yaml=join("config", "project_configuration.yaml")
+    )
---
+++ whosonfirst.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+"""
+Who's On First:
+------------
+
+Reads Who's On First JSON and creates datasets.
+
+"""
+import logging
+from datetime import datetime, timezone
+
+from hdx.data.dataset import Dataset
+from hdx.data.resource import Resource
+from hdx.location.country import Country
+from hdx.utilities.dateparse import parse_date
+from slugify import slugify
+
+logger = logging.getLogger(__name__)
+
+
+class WhosOnFirst:
+    def __init__(self, configuration, retriever, folder, errors):
+        self.configuration = configuration
+        self.retriever = retriever
+        self.folder = folder
+        self.errors = errors
+        self.dataset_data = {}
+
+    def get_data(self, state, datasets=None):
+        inventory_url = self.configuration["inventory_json"]
+        inventory = self.retriever.download_json(inventory_url)
+
+        for entry in inventory:
+            dataset_name = entry["name_compressed"]
+            if len(dataset_name.split("-")) != 5:
+                continue
+
+            if datasets and dataset_name not in datasets:
+                continue
+            last_update_date = entry["last_updated"]
+            if last_update_date:
+                last_update_date = parse_date(last_update_date)
+            else:
+                last_update_date = datetime.now(tz=timezone.utc)
+            if last_update_date > state.get(dataset_name, state["DEFAULT"]):
+                self.dataset_data[dataset_name] = dataset_name
+                state[dataset_name] = last_update_date
+
+        return [{"name": dataset_name} for dataset_name in sorted(self.dataset_data)], state
+
+    def generate_dataset(self, dataset_name):
+        country = dataset_name.split("-")[3]
+        country_info = Country.get_country_info_from_iso2(country)
+        if not country_info:
+            return None
+        country_iso = country_info["#country+code+v_iso3"]
+        country_name = country_info["#country+name+preferred"]
+
+        name = f"whosonfirst-data-admin-{country_iso}"
+        title = f"{country_name}: Who's On First Administrative Subdivisions and Human Settlements"
+        dataset = Dataset({"name": slugify(name), "title": title})
+        # dataset.set_maintainer("")
+        # dataset.set_organization("")
+        dataset.set_subnational(True)
+        dataset.add_country_location(country_name)
+        start_date = parse_date("2015-08-18")
+        dataset.set_time_period(start_date, ongoing=True)
+        dataset.add_tags(
+            [
+                "geodata", "populated places-settlements", "administrative boundaries-divisions", "population"
+            ]
+        )
+        resource = Resource(
+            {
+                "name": dataset_name,
+                "description": f"Administrative polygons for {country_name}",
+                "format": "SHP",
+                "url": f"https://data.geocode.earth/wof/dist/shapefile/{dataset_name}",
+            }
+        )
+        dataset.add_update_resource(resource)
+
+        return dataset
